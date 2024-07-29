c1d9c320eb020019a371e64d8779cd9b74c9778a
Dickson Amenyo <36271402+dkerobean@users.noreply.github.com>
2024-07-29 11:32:45 +0000

    bug fixes for budget edit

--- backend/api/budget/urls.py
+++ backend/api/budget/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('all/', views.BudgetView.as_view(), name="budget-view"),
-    path('edit/<int:pk>/', views.BudgetView.as_view, name="edit")
+    path('edit/<int:pk>/', views.BudgetView.as_view(), name="edit"),
+    path('detail/<int:pk>/', views.BudgetDetailView.as_view(), name="budget-details"),
 ]
 
--- backend/api/budget/views.py
+++ backend/api/budget/views.py
@@ -5,10 +5,16 @@ from .serializers import BudgetSerializer
 from rest_framework.permissions import IsAuthenticated
 from user.models import Budget
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 
 class BudgetView(APIView):
     permission_classes = [IsAuthenticated]
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
     def get(self, request):
         budgets = Budget.objects.all()
@@ -28,12 +34,21 @@ class BudgetView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_401_BAD_REQUEST)
 
     def delete(self, request, pk):
         budget = get_object_or_404(Budget, id=pk, user=request.user)
         budget.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BudgetDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        budget = get_object_or_404(Budget, id=pk, user=request.user)
+        serializer = BudgetSerializer(budget)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 
